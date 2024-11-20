62c7ee179c1e1416b7fad40ff0e8565feee5774c
iamsarb001 <sarbbsandhu555@gmail.com>
2024-11-20 14:24:22 +0530

    design completed - unperfect

--- src/components/Mainview.jsx
+++ src/components/Mainview.jsx
@@ -1,10 +1,10 @@
-import { Dropdownimg, Questionimg, Whitequestionlogo } from "./Images/AllImages"
+import { Downloadimg, Dropdownimg, Greensuccessimg, Magnifyglassicon, Processingimg, Questionimg, TwosidedArrowimg, Whitequestionlogo } from "./Images/AllImages"
 
 
 
 export const Mainview = () => {
     return (
-        <div className="pt-3 pl-8 pr-8 pb-3 flex flex-col gap-8 bg-slate-400 ">
+        <div className="pt-3 pl-8 pr-8 pb-2 flex flex-col gap-4 bg-slate-100 ">
 
             <div className="grid grid-cols-[7fr,1fr] justify-between">
            
@@ -25,13 +25,81 @@ export const Mainview = () => {
                 <CardsLayout /> 
             </div>
 
-            <div>
-                 Payouts 
+            <div className="grid gap-5">
+                <div>  Payouts  </div>
+
+                <div className="pt-3 pr-3 pb-24 pl-3 bg-white rounded-lg ">
+
+                    <div className="flex justify-between">
+                        
+                        <div className="grid grid-cols-[1fr,7fr] items-center bg-[#F2F2F2] border-[#d9d9d9] border-[1px] rounded-md pt-[9px] pb-[9px] pl-4 pr-4">
+                            <div> <Magnifyglassicon /> </div> 
+                            <input type = "text" placeholder="Order ID or transaction ID" style = {{backgroundColor:'#F2F2F2'}} />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-4 justify-center items-end ">
+                            <div className="grid grid-cols-2 bg-white border-[#d9d9d9] border-[1px] rounded-[4px] " > 
+                                    <div className="text-[15px] p-1">  Sort  </div>   
+                                    <div className="flex justify-center items-center">  <TwosidedArrowimg /> </div>  
+                                </div>
+                                <div className="bg-white border-[#d9d9d9] border-[1px] flex h-3/4 w-2/5 justify-center items-center rounded-sm ">  <Downloadimg />  </div>
+                        </div>
+
+
+                    </div>
+
+                    <div className="pt-3  w-full"> 
+                    
+                        <thead>
+                            <tr className="bg-gray-300 grid grid-cols-5">  
+                                <th className="py-[10px] px-3">  OrderID </th>
+                                <th className="py-[10px] px-3">  Status </th>
+                                <th className="py-[10px] px-3">  TransactionID </th>
+                                <th className="py-[10px] px-3">  Refund date </th>
+                                <th className="py-[10px] px-3">  Order amount </th>
+                            </tr>
+                        </thead>
+                
+                        <tbody>
+                            {TableData?.map(i => {
+                                        return (
+                                            <tr className="grid grid-cols-5">
+                                                <td className="p-[14px] flex justify-center"> {i?.Ordervalue} </td>
+                                                <td className="p-[14px] flex  justify-center items-center gap-2"> 
+                                                    <div>
+                                                    {i?.status === "Successful" ? <Greensuccessimg /> : <Processingimg /> }
+                                                    </div>
+                                                    <div className="flex justify-center"> 
+                                                     {i?.status} 
+                                                    </div>
+                                                     </td>
+                                                <td className="p-[14px] flex justify-center"> {i?.transid} </td>
+                                                <td className="p-[14px] flex justify-center"> {i?.rfdate} </td>
+                                                <td className="p-[14px] flex justify-center"> ₹{i?.Orderamount} </td>
+                                            </tr>
+                                        )
+                            })}
+                        </tbody>
+            
+                    </div>
+
+                </div>
             </div>
 
         </div>
     )
 }
+
+export const TableData = [
+    { Ordervalue : "#281209" ,status : 'Successful', transid : "13163445747", rfdate : "Today,08:45 PM", Orderamount : "1125.00" },
+    { Ordervalue : "#281209" ,status : 'Processing', transid : "13163445747", rfdate : "Yesterday,3:00 PM", Orderamount : "1125.00" },
+    { Ordervalue : "#281209" , status : 'Successful', transid : "13163445747", rfdate : "12 Jul 2023,03:00 PM", Orderamount : "1125.00" },
+    { Ordervalue : "#281209" ,status : 'Successful', transid : "13163445747", rfdate : "12 Jul 2023,03:00 PM", Orderamount : "1125.00" },
+    { Ordervalue : "#281209" ,status : 'Successful', transid : "13163445747", rfdate : "12 Jul 2023,03:00 PM", Orderamount : "1125.00" },
+    { Ordervalue : "#281209" ,status : 'Successful', transid : "13163445747", rfdate : "12 Jul 2023,03:00 PM", Orderamount : "1125.00" },
+]
+
+
 
 export const CardsData = [
     { text: "NEXT PAYOUT",price  : "₹2,312.23" , orderscount : '23orders' ,  date : 'by Today.04:00 PM' , bgcolor : '#146EB4' ,color : 'white' },
@@ -46,7 +114,7 @@ export const CardsLayout = () => {
         
             {CardsData?.map(i => {
                 return (
-                    <div key = {i?.text} className="grid grid-rows-2 gap-3 p-3  rounded-md " style = {{backgroundColor : `${i?.bgcolor}` ,color : `${i?.color}` }}> 
+                    <div key = {i?.text} className="grid grid-rows-2 gap-3 p-3 pt-0  rounded-md " style = {{backgroundColor : `${i?.bgcolor}` ,color : `${i?.color}` }}> 
                        
                         <div className="grid grid-cols-2 justify-between">
 
